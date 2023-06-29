95aca11065e436bc9ccb7eeadc109f1790b28ee2
Viktor N <viktor.nestorov@web.de>
2023-06-29 05:21:59 +0200

    Changes in routes

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,74 +1,141 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
+
 const mongoose = require("mongoose");
 
 const Event = require("../models/Event.model");
 const Book = require("../models/Book.model");
 
+
+
 //  POST /api/events  -  Creates a new event
 router.post("/events", (req, res, next) => {
-  const { title, location, address, description, time, date, comments: [],  books: [] } = req.body;
+    const { title, location, address, description, time, date} = req.body;
 
-  Event.create({title, location, address, description, time, date, comments: [],  books: [] })
-    .then((response) => res.json(response))
-    .catch((err) => res.json(err));
+    const newEvent = {
+      title: title,
+      location: location,
+      address: address,
+      description: description,
+      time: time,
+      date: date,
+      comments: [],
+      books: []
+
+    }
+
+    Event.create(newEvent)
+        .then(response => res.status(201).json(response))
+        .catch(err => {
+            console.log("error creating a new event", err);
+            res.status(500).json({
+                message: "error creating a new event",
+                error: err
+            });
+        })
 });
 
-//  GET /api/events -  Retrieves all of the events
-router.get("/events", (req, res, next) => {
-  Event.find()
-    .populate("books")
-    .then((allEvents) => res.json(allEvents))
-    .catch((err) => res.json(err));
+
+
+// GET /api/events -  Retrieves all of the events
+router.get('/events', (req, res, next) => {
+    Event.find()
+        .populate("books")
+        .then(response => {
+            res.json(response)
+        })
+        .catch(err => {
+            console.log("error getting list of events", err);
+            res.status(500).json({
+                message: "error getting list of events",
+                error: err
+            });
+        })
 });
 
-//  GET /api/events/:eventId -  Retrieves a specific event by id
-router.get("/events/:eventId", (req, res, next) => {
-  const { eventId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(eventId)) {
-    res.status(400).json({ message: "Specified id is not valid" });
-    return;
-  }
 
-  // Each event document has `books` array holding `_id`s of book documents
-  // We use .populate() method to get swap the `_id`s for the actual book documents
-  Event.findById(eventId)
-    .populate("books")
-    .then((event) => res.status(200).json(event))
-    .catch((error) => res.json(error));
+
+//  GET /api/events/:eventId  -  Get details of a specific event by id
+router.get('/events/:eventId', (req, res, next) => {
+    
+    const { eventId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(eventId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+
+    Event.findById(eventId)
+        .populate('books')
+        .then(event => res.json(event))
+        .catch(err => {
+            console.log("error getting details of an event", err);
+            res.status(500).json({
+                message: "error getting details of an preventoject",
+                error: err
+            });
+        })
 });
 
-// PUT  /api/events/:eventId  -  Updates a specific event by id
-router.put("/event/:eventId", (req, res, next) => {
-  const { eventId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(eventId)) {
-    res.status(400).json({ message: "Specified id is not valid" });
-    return;
-  }
+// PUT /api/events/:eventId  -  Updates a specific event by id
+router.put('/events/:eventId', (req, res, next) => {
+    const { eventId } = req.params;
 
-  Event.findByIdAndUpdate(eventId, req.body, { new: true })
-    .then((updatedEvent) => res.json(updatedEvent))
-    .catch((error) => res.json(error));
+    if (!mongoose.Types.ObjectId.isValid(eventId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    const newDetails = {
+        title: req.body.title,
+        location: req.body.location,
+        address: req.body.address,
+        description: req.body.description,
+        time: req.body.time,
+        date: req.body.date,
+        
+    }
+
+    Event.findByIdAndUpdate(eventId, newDetails, { new: true })
+        .then((updatedEvent) => res.json(updatedevent))
+        .catch(err => {
+            console.log("error updating event", err);
+            res.status(500).json({
+                message: "error updating event",
+                error: err
+            });
+        })
 });
 
-// DELETE  /api/events/:eventId  -  Deletes a specific event by id
-router.delete("/events/:eventId", (req, res, next) => {
-  const { eventId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(eventId)) {
-    res.status(400).json({ message: "Specified id is not valid" });
-    return;
-  }
 
-  Event.findByIdAndRemove(eventId)
-    .then(() =>
-      res.json({
-        message: `Event with ${eventId} is removed successfully.`,
-      })
-    )
-    .catch((error) => res.json(error));
+// DELETE /api/events/:eventId  -  Delete a specific event by id
+router.delete('/events/:eventId', (req, res, next) => {
+    const {eventId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(eventId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    Event.findByIdAndRemove(eventId)
+        .then(deletedEvent => {
+            return Book.deleteMany({ _id: { $in: deletedEvent.books } });
+        })
+        .then(() => res.json({ message: `Event with id ${eventId} & all associated books were removed successfully.` }))
+        .catch(err => {
+            console.log("error deleting event", err);
+            res.status(500).json({
+                message: "error deleting event",
+                error: err
+            });
+        })
 });
+
+
+
+
 
 module.exports = router;
