1273ae9c52ddc6253a0f0773031f050f2fa48493
JCouderc87 <jeremy.couderc@yahoo.fr>
2023-07-03 16:17:44 +0200

    isathenticated on book and event routers

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const eventRouter = require("./routes/event.routes");
 app.use("/api", isAuthenticated, eventRouter);
 
 const bookRouter = require("./routes/book.routes");
-app.use("/api", bookRouter);
+app.use("/api", isAuthenticated, bookRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
